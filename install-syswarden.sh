8496214fafef89e7e544bac91ae62ce8b54ac5d6
Laurent M. <laurent@data-shield.eu>
2026-02-21 00:13:58 +0100

    ⚡ [UPDATE] versioning 8.00 ⚡

--- install-syswarden.sh
+++ install-syswarden.sh
@@ -16,7 +16,7 @@ LOG_FILE="/var/log/syswarden-install.log"
 CONF_FILE="/etc/syswarden.conf"
 SET_NAME="syswarden_blacklist"
 TMP_DIR=$(mktemp -d)
-VERSION="v7.00"
+VERSION="v8.00"
 SYSWARDEN_DIR="/etc/syswarden"
 WHITELIST_FILE="$SYSWARDEN_DIR/whitelist.txt"
 BLOCKLIST_FILE="$SYSWARDEN_DIR/blocklist.txt"
@@ -2948,7 +2948,7 @@ fi
 if [[ "$MODE" != "update" ]]; then
     clear
     echo -e "${GREEN}#############################################################"
-    echo -e "#     SysWarden Tool Installer (Universal v7.00)     #"
+    echo -e "#     SysWarden Tool Installer (Universal v8.00)     #"
     echo -e "#############################################################${NC}"
 fi
 
